26c28f453aec3f42a659021c8e4e572767cfcf3b
Abhay Singh Chauhan <54814128+chauhanabhay1248@users.noreply.github.com>
2022-01-24 00:05:58 +0530

    inb

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,4 +17,6 @@ const UserSchema = new Schema({
     }
 });
 
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+User.createIndexes();
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 
-// Create a user using: POST "/api/auth". Doesn't require Auth
-router.post('/', (req, res) => {
-    console.log(req.body);
+// Create a user using: POST "/api/auth/createuser". Doesn't require Auth
+router.post('/createuser', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })
+    ], async (req, res) => {
 
-    const user = User(req.body);
-    user.save();
+        // Validation Check
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
 
-    res.send(req.body);
-})
+        try {
+            // Check for user with this email
+            let user = await User.findOne({email: req.body.email});
+            if(user) {
+                return res.status(400).json({error: "User with this email already exists"})
+            }
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+            res.json(user)
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Some error occured");
+        }
+    })
 
 module.exports = router
